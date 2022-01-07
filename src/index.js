eeafc93502e65b586c544b4626905d99d1837b0b
mnzlucas <mnzlucas7@gmail.com>
2022-01-06 21:51:43 -0300

    middleware

--- src/index.js
+++ src/index.js
@@ -7,19 +7,34 @@ const { v4: uuidV4 } = require("uuid");
 const app = express();
 
 const customers = [];
+
+//Middleware
+
+function CheckAccountExistsByCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+  return next();
+}
 app.use(express.json());
 
 
 
 app.post("/account", (request, response) => {
-  const {cpf, name} = request.body;
+  const { cpf, name } = request.body;
 
   const customerAlreadyExists = customers.some(
     (customers) => customers.cpf === cpf
   );
 
-  if (customerAlreadyExists ) {
-    return response.status(400).json({ error: "Customer already exists!"})
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" })
   }
 
   customers.push({
@@ -31,24 +46,36 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/courses", (request, response) => {
-  return response.json(["Curso1","Curso2","Curso3"]);
+// app.get("/statement/:cpf", (request, response) => {
+//   const {cpf} = request.params;
+
+// no Insominia passar o cpf pelo header
+
+// app.use(CheckAccountExistsByCPF);
+app.get("/statement", CheckAccountExistsByCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer.statement);
 });
 
-app.post("/courses", (request, response) => {
-  return response.json(["Curso1", "Curso2", "Curso3", "Curso4"]);
-});
+// app.get("/courses", (request, response) => {
+//   return response.json(["Curso1","Curso2","Curso3"]);
+// });
 
-app.put("/courses/:id", (request, response) => {
-  return response.json(["Curso5", "Curso2", "Curso3", "Curso4"]);
-});
+// app.post("/courses", (request, response) => {
+//   return response.json(["Curso1", "Curso2", "Curso3", "Curso4"]);
+// });
 
-app.patch("/courses/:id", (request, response) => {
-  return response.json(["Curso5", "Curso2", "Curso3", "Curso4"]);
-});
+// app.put("/courses/:id", (request, response) => {
+//   return response.json(["Curso5", "Curso2", "Curso3", "Curso4"]);
+// });
 
-app.delete("/courses/:id", (request, response) => {
-  return response.json(["Curso5", "Curso2", "Curso3", "Curso4"]);
-});
+// app.patch("/courses/:id", (request, response) => {
+//   return response.json(["Curso5", "Curso2", "Curso3", "Curso4"]);
+// });
+
+// app.delete("/courses/:id", (request, response) => {
+//   return response.json(["Curso5", "Curso2", "Curso3", "Curso4"]);
+// });
 
 app.listen(3333);
