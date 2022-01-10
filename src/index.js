83a95426968e75976d23c4132971a2469472c13c
mnzlucas <mnzlucas7@gmail.com>
2022-01-10 16:44:22 -0300

    deposit route

--- src/index.js
+++ src/index.js
@@ -58,6 +58,24 @@ app.get("/statement", CheckAccountExistsByCPF, (request, response) => {
   return response.json(customer.statement);
 });
 
+
+app.post("/deposit", CheckAccountExistsByCPF, (request, response ) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+
+});
 // app.get("/courses", (request, response) => {
 //   return response.json(["Curso1","Curso2","Curso3"]);
 // });
